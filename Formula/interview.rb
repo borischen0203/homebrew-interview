ed6203daf4392a2a09b7466d93b08a47ce6dcd7a
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-30 06:45:13 +0100

    Brew formula update for interview version v0.0.4

--- Formula/interview.rb
+++ Formula/interview.rb
@@ -5,29 +5,29 @@
 class Interview < Formula
   desc "A tool to generate interview question!"
   homepage "https://github.com/borischen0203/interview"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/borischen0203/interview/releases/download/v0.0.3/interview_0.0.3_macOS-64bit.tar.gz"
-      sha256 "912780b7b13372b3fdbe28a2b4d632cf7e6e1fe8219bc65a9ba8b6944de0f908"
+      url "https://github.com/borischen0203/interview/releases/download/v0.0.4/interview_0.0.4_macOS-64bit.tar.gz"
+      sha256 "9d7a3d73349064ef0697272144607726aede31cbe66b8b46e57a42de7d0ec5de"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/borischen0203/interview/releases/download/v0.0.3/interview_0.0.3_macOS-ARM64.tar.gz"
-      sha256 "0d2c520a43f953e5221c20004a360a531abf84507fce5f933aedcb0448034be0"
+      url "https://github.com/borischen0203/interview/releases/download/v0.0.4/interview_0.0.4_macOS-ARM64.tar.gz"
+      sha256 "131e3c2a65b29696d7124e504ae1b4ad90e30063ba79f4b436dbd4ed68706b41"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/borischen0203/interview/releases/download/v0.0.3/interview_0.0.3_Linux-64bit.tar.gz"
-      sha256 "3a3a0beaf7d12045e2f7ca660800ab97ecb1b8f9c948870143224fd3a5c83a0f"
+      url "https://github.com/borischen0203/interview/releases/download/v0.0.4/interview_0.0.4_Linux-64bit.tar.gz"
+      sha256 "bc521f1887c805b88273cca95bf6da1278bd2dd7547f088b1947236665be0939"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/borischen0203/interview/releases/download/v0.0.3/interview_0.0.3_Linux-ARM64.tar.gz"
-      sha256 "b56e27a73445d9b9540a3d6bce4c13a251d31c8bc2d6093f24016312329b262f"
+      url "https://github.com/borischen0203/interview/releases/download/v0.0.4/interview_0.0.4_Linux-ARM64.tar.gz"
+      sha256 "dc9bcc170bca07a3a503e9f87adfbdeee02ec76a1afe2580587c55295f8445df"
     end
   end
 
